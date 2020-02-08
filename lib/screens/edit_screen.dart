d1ad8040ca91c25b7f415f02df42223c7718a911
SardineTa23 <kensho.iwashita.gude@gmail.com>
2020-02-09 01:44:26 +0900

    Home edit indexページの見た目完成

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_own_flashcard/screens/word_list_screen.dart';
 
 class EditScreen extends StatefulWidget {
   @override
@@ -6,33 +7,47 @@ class EditScreen extends StatefulWidget {
 }
 
 class _EditScreenState extends State<EditScreen> {
+
   TextEditingController questionController = TextEditingController();
+  TextEditingController answerController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("新しい単語の登録"),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 30.0,
+    return WillPopScope(
+      onWillPop: () => _backToWordListScreen(),
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("新しい単語の登録"),
+            centerTitle: true,
+          ),
+          //キーボードが出た時の画面で画面が自動スクロールするようになる。
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 30.0,
+                ),
+                Center(
+                  child: Text(
+                    "問題とこたえを入力して「登録」ボタンを押してください",
+                    style: TextStyle(fontSize: 12.0),
+                  ),
+                ),
+                //問題入力部分
+                SizedBox(
+                  height: 30.0,
+                ),
+                //問題入力部分
+                _questionInputPart(),
+                SizedBox(
+                  height: 50.0,
+                ),
+                //答え入力部分
+                _answerInputPart()
+              ],
             ),
-            Center(
-              child: Text(
-                "問題とこたえを入力して「登録」ボタンを押してください",
-                style: TextStyle(fontSize: 12.0),
-              ),
-            ),
-            //問題入力部分
-            SizedBox(
-              height: 30.0,
-            ),
-            _questionInputPart(),
-          ],
-        ));
+          )),
+    );
   }
 
   Widget _questionInputPart() {
@@ -53,5 +68,31 @@ class _EditScreenState extends State<EditScreen> {
         ],
       ),
     );
+  }
+
+  Widget _answerInputPart() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "こたえ",
+            style: TextStyle(fontSize: 24.0),
+          ),
+          TextField(
+            controller: answerController,
+            keyboardType: TextInputType.text,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30.0),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<bool> _backToWordListScreen() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => WordListScreen()));
+    return Future.value(false);
   }
 }
